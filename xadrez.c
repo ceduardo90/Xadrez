c6707e51d2b018c0772812553dc7df65d60d38b2
ceduardo90 <ceduardoprog.github@gmail.com>
2025-10-18 18:52:54 +0000

    nível mestre finalizado

--- xadrez.c
+++ xadrez.c
@@ -1,83 +1,68 @@
 //Simulação de movimento de peças de xadrez
 #include <stdio.h>
 
-    //Mover o cavalo duas casas para cima e uma para a direita
-    int main(){
-
-    printf("Cavalo: \n");
-    
-    int movimentocompleto = 1; //Controle do movimento em "L"
-
-    while (movimentocompleto--)
-    {
-      for (int cavalo = 0; cavalo < 2; cavalo++)
-        {
-            printf("Cima!\n"); //Imprime "Cima" duas vezes
-        }
-        printf("Direita!\n"); //Imprime "Direita" uma vez
-
-    }
-    
-    return 0;
-
-    }
-
     //Mover a torre 5 casas para a direita
-    void moverTorre(int casas) {
-
-    printf("Torre:\n");
+    void moverTorre(int casas) { //Função recursiva
 
         if (casas > 0)
         {
-            printf("Direita!\n");
-            moverTorre(casas - 1);
+            printf("Direita!\n"); //Imprime "Direita" 5 vezes
+            moverTorre(casas - 1); //Chama a si mesmo com casas - 1
         }
-    }
-
-    int main() {
-        moverTorre(5);
-        return 0;
-
-    printf("\n");
 
     }
 
     //Mover o bispo 5 casas na diagonal
-    void moverBispo(int casas) {
+    void moverBispo(int casas2) { //Função recursiva
 
-    printf("Bispo:\n");
-
-        if (casas > 0)
+        if (casas2 > 0)
         {
-            printf("Cima, Direita!\n");
-            moverBispo(casas - 1);
+            printf("Cima, Direita!\n"); //Imprime "Cima, Direita" 5 vezes 
+            moverBispo(casas2 - 1); //Chama a si mesmo com casas2 - 1
         }
+
     }
 
-    int main() {
-        moverBispo(5);
-        return 0;
+    //Mover a rainha 8 casas para a esquerda
+    void moverRainha(int casas3) { //Função recursiva
 
-    printf("\n");
+        if (casas3 > 0)
+        {
+            printf("Esquerda!\n"); //Imprime "Esquerda" 8 vezes
+            moverRainha(casas3 - 1); //Chama a si mesmo com casas3 - 1
+        }
 
     }
     
-    //Mover a rainha 8 casas para a esquerda
-    void moverRainha(int casas) {
-
-    printf("Rainha:\n");
-
-        if (casas > 0)
-        {
-            printf("Esquerda!\n");
-            moverTorre(casas - 1);
-        }
-    }
-
     int main() {
-        moverRainha(8);
-        return 0;
 
+    printf("Torre:\n");
+    moverTorre(5);
     printf("\n");
 
+    printf("Bispo:\n");
+    moverBispo(5);
+    printf("\n");
+
+    printf("Rainha:\n");
+    moverRainha(8);
+    printf("\n");
+
+    //Mover o cavalo duas casas para cima e uma para a direita
+    printf("Cavalo:\n");
+    
+    int movimentoCompleto = 1; //Controle do movimento em "L"
+
+      while (movimentoCompleto--)
+      {
+        for (int cavalo = 0; cavalo < 2; cavalo++)
+        {
+            printf("Cima!\n"); //Imprime "Cima" duas vezes
+        }
+        printf("Direita\n"); //Imprime "Direita" uma vez
+      }
+    
+    return 0;
+
     }
+    
